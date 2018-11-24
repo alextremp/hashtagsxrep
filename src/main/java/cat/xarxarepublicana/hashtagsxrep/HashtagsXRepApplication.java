567df167089390f08f4bc3d5d0b22df31a0f099c
alextremp <alex.castells@schibsted.com>
2018-11-25 00:02:49 +0100

    First commit

---
+++ src/main/java/cat/xarxarepublicana/hashtagsxrep/HashtagsXRepApplication.java
@@ -0,0 +1,12 @@
+package cat.xarxarepublicana.hashtagsxrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtagsXRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashtagsXRepApplication.class, args);
+	}
+}
