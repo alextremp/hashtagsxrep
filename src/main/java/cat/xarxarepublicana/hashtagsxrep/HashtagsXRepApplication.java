0956e4b790b2eaa46fbc34efb9a97e65e400dfe1
alextremp <alex.castells@schibsted.com>
2018-12-13 21:07:44 +0100

    force timezone

--- src/main/java/cat/xarxarepublicana/hashtagsxrep/HashtagsXRepApplication.java
+++ src/main/java/cat/xarxarepublicana/hashtagsxrep/HashtagsXRepApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import java.util.Collections;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class HashtagsXRepApplication {
 
 	public HashtagsXRepApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
 		freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(Collections.singletonList("/META-INF/security.tld"));
 	}
 
