c061314c8b97edbee951d0d831db45d933735da8
Alex Castells <alex.castells@scmspain.com>
2019-01-03 04:40:07 +0100

    Feature/performance caching (#26)
    * caching listmonitors usecase entire response
    
    * Cached user repository + using caffeine cache
    
    * Cache hashtag report

--- src/main/java/cat/xarxarepublicana/hashtagsxrep/infrastructure/configuration/UseCaseConfiguration.java
+++ src/main/java/cat/xarxarepublicana/hashtagsxrep/infrastructure/configuration/UseCaseConfiguration.java
@@ -17,6 +17,8 @@ import cat.xarxarepublicana.hashtagsxrep.domain.poll.ProposalFactory;
 import cat.xarxarepublicana.hashtagsxrep.domain.report.ReportRepository;
 import cat.xarxarepublicana.hashtagsxrep.domain.twitter.TwitterRepository;
 import cat.xarxarepublicana.hashtagsxrep.domain.user.UserRepository;
+import cat.xarxarepublicana.hashtagsxrep.infrastructure.cache.CachedGetTwitterQueryReportUseCase;
+import cat.xarxarepublicana.hashtagsxrep.infrastructure.cache.CachedListMonitorUseCase;
 import cat.xarxarepublicana.hashtagsxrep.infrastructure.security.AuthenticationContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -46,8 +48,8 @@ public class UseCaseConfiguration {
     }
 
     @Bean
-    public ListMonitorUseCase listMonitorUseCase(MonitorRepository monitorRepository) {
-        return new ListMonitorUseCase(monitorRepository);
+    public CachedListMonitorUseCase listMonitorUseCase(MonitorRepository monitorRepository) {
+        return new CachedListMonitorUseCase(monitorRepository);
     }
 
     @Bean
@@ -56,8 +58,8 @@ public class UseCaseConfiguration {
     }
 
     @Bean
-    public GetTwitterQueryReportUseCase getTwitterQueryReportUseCase(ReportRepository reportRepository, MonitorRepository monitorRepository) {
-        return new GetTwitterQueryReportUseCase(reportRepository, monitorRepository);
+    public CachedGetTwitterQueryReportUseCase getTwitterQueryReportUseCase(ReportRepository reportRepository, MonitorRepository monitorRepository) {
+        return new CachedGetTwitterQueryReportUseCase(reportRepository, monitorRepository);
     }
 
     @Bean
